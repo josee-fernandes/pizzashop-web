48b8631f6427153660b00d4f4f28bfff306afa9f
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2024-01-26 19:18:34 -0300

    Popular products graph

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -54,6 +54,8 @@ export function RevenueChart() {
                 })
               }
             />
+            <CartesianGrid vertical={false} className="stroke-muted" />
+
             <Line
               type="linear"
               strokeWidth={2}
